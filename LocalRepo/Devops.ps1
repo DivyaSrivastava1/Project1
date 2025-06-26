f1ae9637075a58fb36520793eb27b294271c5580
DivyaSrivastava1 <divyasrivastava689@gmail.com>
2025-06-26 14:19:40 +0200

    modified Docker and jenkins file

--- LocalRepo/Devops.ps1
+++ LocalRepo/Devops.ps1
@@ -1,4 +1,2 @@
-# Open Command Prompt and check running services
+# Open Command Prompt and check all the services
 Start-Process cmd.exe -ArgumentList "/k sc query state= all"
-# Open Command Prompt with a list of running services (formatted)
-#Start-Process cmd.exe -ArgumentList "/k echo Listing all running services... && echo ---------------------------- && sc query state= all | findstr /I /C:"RUNNING" && echo ---------------------------- && pause"
